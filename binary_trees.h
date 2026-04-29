df9f9eb67690d630c03fc4295ef4089dc3a2e11c
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-04-29 17:19:42 +0000

    Add binary_tree_inorder prototype to header

--- binary_trees.h
+++ binary_trees.h
@@ -15,10 +15,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -27,6 +27,8 @@ typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
